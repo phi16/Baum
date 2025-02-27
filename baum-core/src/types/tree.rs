14320ece053e20ab1551f2c8015cd321455c5db9
phi16 <phi16.256@gmail.com>
2025-02-27 14:05:15 +0900

    wandering

--- baum-core/src/types/tree.rs
+++ baum-core/src/types/tree.rs
@@ -2,35 +2,58 @@ use std::collections::HashMap;
 use std::rc::Rc;
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
-pub struct Id(pub u32);
+pub struct DefId(pub u32);
 
-impl std::fmt::Debug for Id {
+impl std::fmt::Debug for DefId {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "@{}", self.0)
+  }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub struct LocalId(pub u32);
+
+impl std::fmt::Debug for LocalId {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "%{}", self.0)
+  }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub struct TagId(pub u32);
+
+impl std::fmt::Debug for TagId {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     write!(f, "#{}", self.0)
   }
 }
 
 #[derive(Debug, Clone)]
-pub enum ExprF<I, E> {
+pub enum ExprF<DefI, LocI, TagI, E> {
   Hole,
-  Var(I),
+  Var(LocI),
   Ann(E, E),
   Uni,
 
-  Pi(Option<I>, E, E),
-  Lam(I, E, E),
+  Def(DefI),
+  Let(Vec<(DefI, E)>, E),
+
+  Pi(Option<LocI>, E, E),
+  Lam(LocI, E, E),
   App(E, E),
 
-  Sigma(Vec<(I, E)>),
-  Obj(Vec<(I, E)>),
-  Prop(I, E),
+  Sigma(Vec<(TagI, Option<LocI>, E)>),
+  Obj(Vec<(TagI, E)>),
+  Prop(TagI, E),
 }
 
 #[derive(Debug, Clone)]
-pub struct Expr(pub ExprF<Id, Rc<Expr>>);
+pub struct Expr(pub ExprF<DefId, LocalId, TagId, Rc<Expr>>);
 
 #[derive(Debug, Clone)]
 pub struct Program {
-  pub decls: Vec<(Id, Rc<Expr>)>,
-  pub symbols: HashMap<Id, String>,
+  pub decls: Vec<(DefId, Rc<Expr>)>,
+  pub def_symbols: HashMap<DefId, String>,
+  pub local_symbols: HashMap<LocalId, String>,
+  pub tag_symbols: HashMap<TagId, String>,
 }
