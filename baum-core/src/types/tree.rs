017ed35ec814b146cce36a0bbed168ed442fc950
phi16 <phi16.256@gmail.com>
2025-02-27 21:17:12 +0900

    rename

--- baum-core/src/types/tree.rs
+++ baum-core/src/types/tree.rs
@@ -11,25 +11,25 @@ impl std::fmt::Debug for DefId {
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
-pub struct LocalId(pub u32);
+pub struct BindId(pub u32);
 
-impl std::fmt::Debug for LocalId {
+impl std::fmt::Debug for BindId {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     write!(f, "%{}", self.0)
   }
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
-pub struct TagId(pub u32);
+pub struct NameId(pub u32);
 
-impl std::fmt::Debug for TagId {
+impl std::fmt::Debug for NameId {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     write!(f, "#{}", self.0)
   }
 }
 
 #[derive(Debug, Clone)]
-pub enum ExprF<DefI, LocI, TagI, E> {
+pub enum ExprF<DefI, LocI, NameI, E> {
   Hole,
   Var(LocI),
   Ann(E, E),
@@ -42,18 +42,18 @@ pub enum ExprF<DefI, LocI, TagI, E> {
   Lam(LocI, E, E),
   App(E, E),
 
-  Sigma(Vec<(TagI, Option<LocI>, E)>),
-  Obj(Vec<(TagI, E)>),
-  Prop(TagI, E),
+  Sigma(Vec<(NameI, Option<LocI>, E)>),
+  Obj(Vec<(NameI, E)>),
+  Prop(NameI, E),
 }
 
 #[derive(Debug, Clone)]
-pub struct Expr(pub ExprF<DefId, LocalId, TagId, Rc<Expr>>);
+pub struct Expr(pub ExprF<DefId, BindId, NameId, Rc<Expr>>);
 
 #[derive(Debug, Clone)]
 pub struct Program {
   pub decls: Vec<(DefId, Rc<Expr>)>,
   pub def_symbols: HashMap<DefId, String>,
-  pub local_symbols: HashMap<LocalId, String>,
-  pub tag_symbols: HashMap<TagId, String>,
+  pub bind_symbols: HashMap<BindId, String>,
+  pub name_symbols: HashMap<NameId, String>,
 }
