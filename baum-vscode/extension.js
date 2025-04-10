209a6d15653189f6834bd382fb74a8a73b5d5c49
phi16 <phi16.256@gmail.com>
2025-04-10 22:38:32 +0900

    dev update

--- baum-vscode/extension.js
+++ baum-vscode/extension.js
@@ -4,35 +4,73 @@ const lc = require("vscode-languageclient");
 
 let client;
 
-async function restart() {
-  vscode.window.showInformationMessage("baum-vscode restating...");
+function log(s) {
+  vscode.window.showInformationMessage(s);
+}
+
+function logError(s) {
+  vscode.window.showErrorMessage(s);
+}
+
+async function start() {
+  if (client) {
+    logError("baum-vscode already started");
+    return;
+  }
+  const serverOptions = {
+    command: "baum-ls"
+  };
+  const clientOptions = {
+    documentSelector: [{ scheme: "file", language: "baum" }]
+  };
   try {
-    if(client === undefined) {
-      throw new Error();
-    }
-    await client.restart();
+    client = new lc.LanguageClient("baum", "baum-ls-client", serverOptions, clientOptions);
+    await client.start();
+    log("baum-vscode started");
   } catch(e) {
-    vscode.window.showErrorMessage(`Failed to restart baum-ls: ${e}`);
+    logError(`Failed to start baum-ls: ${e}`);
   }
 }
 
-function activate(context) {
+async function restart() {
+  if(!client) {
+    logError("baum-vscode not started yet");
+    return;
+  }
+  try {
+    await client.restart();
+    log("baum-vscode restarted");
+  } catch(e) {
+    logError(`Failed to restart baum-ls: ${e}`);
+  }
+}
+
+async function stop() {
+  if(!client) {
+    logError("baum-vscode already stopped");
+    return;
+  }
+  try {
+    await client.stop();
+    log("baum-vscode stopped");
+    client = undefined;
+  } catch(e) {
+    logError(`Failed to stop baum-ls: ${e}`);
+  }
+}
+
+async function activate(context) {
+  log("baum-vscode activated");
   context.subscriptions.push(
     vscode.commands.registerCommand("baum-vscode.restartServer", () => restart())
   );
-  vscode.window.showInformationMessage("baum-vscode activated");
-  try {
-    const serverOptions = {
-      command: "baum-ls"
-    };
-    const clientOptions = {
-      documentSelector: [{ scheme: "file", language: "baum" }]
-    };
-    client = new lc.LanguageClient("baum", "baum-ls-client", serverOptions, clientOptions);
-    context.subscriptions.push(client.start());
-  } catch(e) {
-    vscode.window.showErrorMessage(`Failed to start baum-ls: ${e}`);
-  }
+  context.subscriptions.push(
+    vscode.commands.registerCommand("baum-vscode.startServer", () => start())
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("baum-vscode.stopServer", () => stop())
+  );
+  await start();
 }
 
 function deactivate() {
